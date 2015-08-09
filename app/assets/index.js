bb68668f7a437c65e212516895dbc998160fd7ec
barr-code <vb329@georgetown.edu>
2015-08-09 21:25:27 +0100

    map location for store

--- app/assets/index.js
+++ app/assets/index.js
@@ -1,10 +1,31 @@
-function showMap(){
+function bindShowMap(){
   $('#show-map').bind('click', function(e){
-    $('#map-container').show();
+    $('#map-container').toggle();
+    setMap();
+    $('html, body').animate({
+        scrollTop: $("#map-container").offset().top
+    }, 1000);
   })
+}
+
+function setMap(){
+  var latlng = new google.maps.LatLng(53.3450044, -6.272482699999955);
+  var myOptions = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false
+  };
+  map = new google.maps.Map($('#map-canvas')[0], myOptions);
+  gMapObj = map;
+
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map
+  });
 }
 
 $(document).ready(function(){
   $('#map-container').hide();
-  showMap();
+  bindShowMap();
 })
